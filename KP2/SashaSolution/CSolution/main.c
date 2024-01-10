276ace11901d493d1fefecabd0716ee1b6b821fe
insiders17 <sasha.adamow05@gmail.com>
2024-01-10 21:11:35 +0200

    change main.c

--- KP2/SashaSolution/CSolution/main.c
+++ KP2/SashaSolution/CSolution/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 
 int main() {
@@ -9,15 +10,21 @@ int main() {
     printf("Enter x value: ");
     scanf("%lf", &x);
 
+
     printf("Enter y value:");
     scanf("%lf", &y);
 
-    if (x > 0 && x * y > 1) 
+
+    if (x > 0 && x * y > 1)
         f = (x + y) / (1 - x * y);
-    else 
+    else if (x == 0)
         f = (pow(x, 2) - y) / (1 - x * y);
+    else {
+        printf("Not value is valid");
+        return EXIT_FAILURE;
+    };
 
-    printf("The function is equal to = %lf", f);
+    printf("The function is equal to = %lf\n", f);
 
-    return 0;
+    return EXIT_SUCCESS;
 }

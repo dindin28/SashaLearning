8f71524695106628d20b6f0e8ef810e721525b63
insiders17 <sasha.adamow05@gmail.com>
2024-01-10 20:04:46 +0200

    fix main.c

--- KP2/SashaSolution/CSolution/main.c
+++ KP2/SashaSolution/CSolution/main.c
@@ -7,16 +7,17 @@ int main() {
     double y, x, f;
 
     printf("Enter x value: ");
-    scanf("%f", &x);
+    scanf("%lf", &x);
 
     printf("Enter y value:");
-    scanf("%f", &y);
+    scanf("%lf", &y);
 
-    if (x > 0, x * y > 1) f = (x + y) / (1 - x * y);
-    else f = (pow(x, 2) - y) / (1 - x * y);
+    if (x > 0 && x * y > 1) 
+    f = (x + y) / (1 - x * y);
+    else 
+    f = (pow(x, 2) - y) / (1 - x * y);
 
-    printf("The function is equal to = %f", f);
-    getch();
+    printf("The function is equal to = %lf", f);
 
     return 0;
 }

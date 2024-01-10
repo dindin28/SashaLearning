79cf7a4db59dc7213121c171e89ad1c519f568c7
insiders17 <sasha.adamow05@gmail.com>
2024-01-10 20:06:45 +0200

    fix main.c(1)

--- KP2/SashaSolution/CSolution/main.c
+++ KP2/SashaSolution/CSolution/main.c
@@ -13,9 +13,9 @@ int main() {
     scanf("%lf", &y);
 
     if (x > 0 && x * y > 1) 
-    f = (x + y) / (1 - x * y);
+        f = (x + y) / (1 - x * y);
     else 
-    f = (pow(x, 2) - y) / (1 - x * y);
+        f = (pow(x, 2) - y) / (1 - x * y);
 
     printf("The function is equal to = %lf", f);
 

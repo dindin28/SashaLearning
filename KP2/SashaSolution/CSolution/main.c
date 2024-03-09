2c2c2a84dfceb1f62ee925822655dc4a0cf12d81
insiders17 <sasha.adamow05@gmail.com>
2024-03-09 19:54:30 +0200

    added pointers for the main function

--- KP2/SashaSolution/CSolution/main.c
+++ KP2/SashaSolution/CSolution/main.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+bool calculateFunction(double x, double y, double* f) {
+    if (x > 0 && (x) * (y) > 1)
+        *f = (x + y) / (1 - (x) * (y));
+    else if (x == 0)
+        *f = (pow(x, 2) - y) / (1 - (x) * (y));
+    else {
+        printf("No valid value\n");
+        return(false);
+    }
+}
 
 int main() {
-    
     double y, x, f;
 
     printf("Enter x value: ");
     scanf("%lf", &x);
 
-
     printf("Enter y value: ");
     scanf("%lf", &y);
 
-
-    if (x > 0 && x * y > 1)
-        f = (x + y) / (1 - x * y);
-    else if (x == 0)
-        f = (pow(x, 2) - y) / (1 - x * y);
-    else {
-        printf("Not value is valid\n");
-        return EXIT_FAILURE;
-    };
+    calculateFunction(&x, &y, &f);
 
     printf("The function is equal to = %lf\n", f);
 

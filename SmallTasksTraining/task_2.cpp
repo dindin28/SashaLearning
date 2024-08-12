7e40782ffc14b66b5c324320a971b8c0d016b275
insiders17 <sasha.adamow05@gmail.com>
2024-08-12 16:41:12 +0300

    change task 2

--- SmallTasksTraining/task_2.cpp
+++ SmallTasksTraining/task_2.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    int sum_a = 0;
     int b = 0;
     int a;
+    int n;
 
-    for (int i = 1; i <= 10; ++i) {
+    do {
+        std::cout << "Enter a positive integer value for 'n': ";
+        std::cin >> n;
+
+        if (n <= 0) {
+            std::cout << "Invalid input. 'n' must be a positive integer.\n";
+        }
+    } while (n <= 0);
+
+
+    for (int i = 1, sum_a = 0; i <= n; ++i) {
         sum_a += i;
-        a = pow(sum_a, 2);
+        if (i == n) {
+            a = pow(sum_a, 2);
+        }
     }
 
-    for (int i = 1; i <= 10; ++i) {
+    for (int i = 1; i <= n; ++i) {
         b += pow(i, 2);
     }
 
